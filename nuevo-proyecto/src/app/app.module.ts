2b5bdf957d7c949e89d8eaec1c318b6d3e8e7f92
Hassan Mafud <hassanmafud_97@hotmail.com>
2020-09-18 19:27:43 -0300

    Agregamos saludador

--- nuevo-proyecto/src/app/app.module.ts
+++ nuevo-proyecto/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SaludadorComponent } from './saludador/saludador.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SaludadorComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
