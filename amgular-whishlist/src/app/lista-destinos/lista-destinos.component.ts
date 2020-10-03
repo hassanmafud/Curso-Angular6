8e01e525ed8aa6b5104834ba818614262e22dd9a
Hassan Mafud <hassanmafud97@gmail.com>
2020-10-02 21:25:56 -0300

    ir

--- amgular-whishlist/src/app/lista-destinos/lista-destinos.component.ts
+++ amgular-whishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -1,5 +1,6 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import {DestinoViaje} from './../models/destino-viaje.model';
+import { DestinoViaje } from './../models/destino-viaje.model';
 
 @Component({
   selector: 'app-lista-destinos',
@@ -7,19 +8,26 @@ import {DestinoViaje} from './../models/destino-viaje.model';
   styleUrls: ['./lista-destinos.component.css']
 })
 export class ListaDestinosComponent implements OnInit {
-	destinos: DestinoViaje[];
+  destinos: DestinoViaje[];
 
-  constructor() { 
-  	this.destinos = [];
+  constructor() {
+    this.destinos = [];
   }
 
   ngOnInit(): void {
   }
 
-  guardar(nombre:string, url:string): boolean{
-  	this.destinos.push(new DestinoViaje(nombre, url));
-  	console.log(this.destinos);
-  	return false;
+  guardar(nombre: string, url: string): boolean {
+    this.destinos.push(new DestinoViaje(nombre, url));
+    console.log(this.destinos);
+    return false;
+  }
+
+  elegido(d: DestinoViaje) {
+    this.destinos.forEach(function (x) {
+      x.setSelected(false);
+    });
+    d.setSelected(true);
   }
 
 }
