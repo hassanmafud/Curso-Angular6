9a7c85b94b0f5914c800ea867c7125952641163f
Hassan Mafud <hassanmafud97@gmail.com>
2020-10-03 20:16:17 -0300

    seguimos avanzando con el curso

--- amgular-whishlist/src/app/destino-viaje/destino-viaje.component.ts
+++ amgular-whishlist/src/app/destino-viaje/destino-viaje.component.ts
@@ -8,9 +8,10 @@ import {DestinoViaje} from './../models/destino-viaje.model';
 })
 export class DestinoViajeComponent implements OnInit {
 	@Input() destino: DestinoViaje;
+	@Input() position: number;
 	@HostBinding('attr.class') cssClass = 'col-md-4';
 	@Output()	clicked: EventEmitter<DestinoViaje>;
-
+	
 	constructor() {
 		this.clicked = new EventEmitter();
 	}
