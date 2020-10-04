db504fa049add793e55d03b209d46ac708e41cbf
Hassan Mafud <hassanmafud97@gmail.com>
2020-10-04 15:02:50 -0300

    Observables

--- amgular-whishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ amgular-whishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,5 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { fromEvent } from 'rxjs';
+import { map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
 import { DestinoViaje } from '../models/destino-viaje.model';
 
 @Component({
@@ -11,6 +14,7 @@ export class FormDestinoViajeComponent implements OnInit {
   @Output() onItemAdded : EventEmitter<DestinoViaje>;
   fg: FormGroup;
   minLongitud= 3 ;
+  searchResults: string [];
 
   constructor(fb: FormBuilder) {
     this. onItemAdded = new EventEmitter();
@@ -19,17 +23,29 @@ export class FormDestinoViajeComponent implements OnInit {
       url: ['']
     });
 
-    this.fg.valueChanges.subscribe((form: any)=> {
+    this.fg.valueChanges.subscribe((form: any) => {
       console.log('cambio el formulario:' , form);
     });
    }
 
   ngOnInit(): void {
+    let elemNombre = <HTMLInputElement>document.getElementById('nombre');
+    fromEvent(elemNombre, 'input')
+    .pipe(
+      map((e: KeyboardEvent) => (e.target as HTMLInputElement).value),
+      filter(text => text.length > 2),
+      debounceTime(200),
+      distinctUntilChanged(),
+      switchMap(() => ajax('/assets/datos.json'))
+    ).subscribe(ajaxResponse => {
+      this.searchResults = ajaxResponse.response;
+    });
+
   }
 
   guardar(nombre: string , url: string): boolean{
     let d = new DestinoViaje(nombre, url);
-    this.onItemAdded.emit(d)
+    this.onItemAdded.emit(d);
     return false;
   }
  
